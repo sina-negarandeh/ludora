7ebb172f0ed3f33d19a5dc3da0ba254e540c52b4
Sina Negarandeh <sina-negarandeh@outlook.com>
2026-08-15 01:24:04 -0400

    Update routing to /games

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GamesList } from './pages/GamesList';
 
@@ -16,7 +16,8 @@ function App() {
         </div>
         <div className="min-h-screen bg-background">
           <Routes>
-            <Route path="/" element={<GamesList />} />
+            <Route path="/" element={<Navigate to="/games" replace />} />
+            <Route path="/games" element={<GamesList />} />
           </Routes>
         </div>
       </BrowserRouter>
